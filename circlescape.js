035de1895638f0ff545e1851b8bd112a57b3f907
Cameron May <camerondmay@gmail.com>
2019-10-27 16:55:02 +1300

    MajorRevision

--- circlescape.js
+++ circlescape.js
@@ -3,80 +3,72 @@ let lineArrayY = [];
 
 let bgLayer1, fgLayer1, subLayer1;
 let bgLayer2, fgLayer2, subLayer2;
+let bgLayer3, fgLayer3, subLayer3;
+let bg;
 
 let drawingIsActive = 1;
 
 let tileNum = 5;
-
 let sliderImg;
 let holdingImg;
 
-let circleIsActive = 1;
+let currentLayer = 1;
 
-function preload(){
-  fgLayer1 = loadImage('assets/background.jpg');
-    fgLayer2 = loadImage('assets/background2.jpg');
+function preload() {
+  fgLayer1 = loadImage('assets/s1-1.jpg');
+  fgLayer2 = loadImage('assets/s1-2.jpg');
+  fgLayer3 = loadImage('assets/s1-3.jpg');
+  bg = loadImage('assets/bg1.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   bgLayer1 = createGraphics(windowWidth, windowHeight);
-
   subLayer1 = createGraphics(windowWidth, windowHeight);
-  bgLayer1.background(appBg);
+  bgLayer1.background(255);
   bgLayer1.strokeWeight(12);
   bgLayer1.stroke(0);
   bgLayer1.noFill();
 
 
   bgLayer2 = createGraphics(windowWidth, windowHeight);
-
   subLayer2 = createGraphics(windowWidth, windowHeight);
-  bgLayer2.background(appBg);
+  bgLayer2.background(255);
   bgLayer2.strokeWeight(12);
   bgLayer2.stroke(0);
   bgLayer2.noFill();
 
+  bgLayer3 = createGraphics(windowWidth, windowHeight);
+  subLayer3 = createGraphics(windowWidth, windowHeight);
+  bgLayer3.background(255);
+  bgLayer3.strokeWeight(12);
+  bgLayer3.stroke(0);
+  bgLayer3.noFill();
 
   sliderImg = createGraphics(windowWidth, windowHeight);
   holdingImg = createGraphics(windowWidth, windowHeight);
 
 
   calcDimensions();
-  saveNext();
+  makeSwatch();
+
 
 }
 
-function calcDimensions() {
-  vW = width / 100;
 
-  if (width > height) {
-    vMax = width / 100;
-    vMin = height / 100;
-  } else {
-    vMax = height / 100;
-    vMin = width / 100;
-  }
-}
-
-
-function keyPressed(){
-  circleIsActive = !circleIsActive;
-}
 
 function touchStarted() {
-// nothing for now
+  // nothing for now
   return false;
 }
 
 function touchMoved() {
 
-  if (drawingIsActive) {
+  if (drawingIsActive && eraseBoolean === 0) {
 
-    if (circleIsActive) {
-
-      bgLayer1.strokeWeight(5);
+    if (currentLayer === 1) {
+      bgLayer1.strokeWeight(12);
       lineArrayX.push(mouseX);
       lineArrayY.push(mouseY);
       bgLayer1.beginShape();
@@ -84,23 +76,60 @@ function touchMoved() {
         bgLayer1.curveVertex(lineArrayX[i], lineArrayY[i]);
       }
       bgLayer1.endShape();
-
-    } else {
-
-      bgLayer1.strokeCap(ROUND);
-      bgLayer2.strokeWeight(50);
-      bgLayer2.line(pwinMouseX, pwinMouseY, winMouseX, winMouseY);
-      bgLayer2.point(winMouseX, winMouseY)
-
-
     }
 
-  } else {
+    else if (currentLayer === 2) {
+        bgLayer2.strokeWeight(12);
+      lineArrayX.push(mouseX);
+      lineArrayY.push(mouseY);
+      bgLayer2.beginShape();
+      for (i = 0; i < lineArrayX.length; i++) {
+        bgLayer2.curveVertex(lineArrayX[i], lineArrayY[i]);
+      }
+      bgLayer2.endShape();
+    }
+
+    else if (currentLayer === 3) {
+        bgLayer3.strokeWeight(12);
+      lineArrayX.push(mouseX);
+      lineArrayY.push(mouseY);
+      bgLayer3.beginShape();
+      for (i = 0; i < lineArrayX.length; i++) {
+        bgLayer3.curveVertex(lineArrayX[i], lineArrayY[i]);
+      }
+      bgLayer3.endShape();
+    }
+
+  }
+  else if (drawingIsActive && eraseBoolean){
+    bgLayer1.noStroke();
+    bgLayer1.fill(255);
+      bgLayer2.noStroke();
+    bgLayer2.fill(255);
+      bgLayer3.noStroke();
+    bgLayer3.fill(255);
+    bgLayer1.circle(winMouseX, winMouseY, 100, 100);
+    bgLayer2.circle(winMouseX, winMouseY, 100, 100);
+    bgLayer3.circle(winMouseX, winMouseY, 100, 100);
+
+  bgLayer1.stroke(0);
+    bgLayer2.stroke(0);
+      bgLayer3.stroke(0);
+    bgLayer1.noFill();
+    bgLayer2.noFill();
+    bgLayer3.noFill();
+
+
+  }
+
+  else {
 
     tileNum = constrain(((width / (mouseX + 20))), 1, 10);
     makeSlider(winMouseX);
 
   }
+
+    touchDraw();
 
   return false;
 
@@ -113,8 +142,8 @@ function touchEnded() {
 
   if (drawingIsActive) {
 
-    if (circleIsActive) {
 
+    if (currentLayer === 1) {
       if (lineArrayX.length > 20 && dist(mouseX, mouseY, lineArrayX[0], lineArrayY[0]) < 50) {
         bgLayer1.fill(0);
         lineArrayX.push(lineArrayX[0]);
@@ -130,36 +159,87 @@ function touchEnded() {
 
       lineArrayX = [];
       lineArrayY = [];
-      bgLayer1.fill(0, 1);
+      bgLayer1.noFill();
     }
 
-    else {
+    else if (currentLayer === 2) {
+      if (lineArrayX.length > 20 && dist(mouseX, mouseY, lineArrayX[0], lineArrayY[0]) < 50) {
+        bgLayer2.fill(0);
+        lineArrayX.push(lineArrayX[0]);
+        lineArrayY.push(lineArrayY[0]);
 
+        bgLayer2.beginShape();
+        for (i = 0; i < lineArrayX.length; i++) {
+          bgLayer2.curveVertex(lineArrayX[i], lineArrayY[i]);
+        }
+        bgLayer2.curveVertex(lineArrayX[0], lineArrayY[0]);
+        bgLayer2.endShape();
+      }
+
+      lineArrayX = [];
+      lineArrayY = [];
+      bgLayer2.noFill();
     }
+
+    else if (currentLayer === 3) {
+      if (lineArrayX.length > 20 && dist(mouseX, mouseY, lineArrayX[0], lineArrayY[0]) < 50) {
+        bgLayer3.fill(0);
+        lineArrayX.push(lineArrayX[0]);
+        lineArrayY.push(lineArrayY[0]);
+
+        bgLayer3.beginShape();
+        for (i = 0; i < lineArrayX.length; i++) {
+          bgLayer3.curveVertex(lineArrayX[i], lineArrayY[i]);
+        }
+        bgLayer3.curveVertex(lineArrayX[0], lineArrayY[0]);
+        bgLayer3.endShape();
+      }
+
+      lineArrayX = [];
+      lineArrayY = [];
+      bgLayer3.noFill();
+    }
+
+
   } else {
     //nothing for now
   }
+
+  touchDraw();
 
   return false;
 
 }
 
 
-function draw() {
+function touchDraw() {
 
   if (drawingIsActive) {
+    blendMode(BLEND);
+    background(255);
     subLayer1.blendMode(BLEND);
     subLayer1.image(bgLayer1, 0, 0, width, height);
     subLayer1.blendMode(LIGHTEST);
     subLayer1.image(fgLayer1, 0, 0, width, height);
+    blendMode(MULTIPLY);
     image(subLayer1, 0, 0, width, height);
 
     subLayer2.blendMode(BLEND);
     subLayer2.image(bgLayer2, 0, 0, width, height);
     subLayer2.blendMode(LIGHTEST);
     subLayer2.image(fgLayer2, 0, 0, width, height);
-    blendMode(DARKEST);
+    blendMode(MULTIPLY);
     image(subLayer2, 0, 0, width, height);
+
+    subLayer3.blendMode(BLEND);
+    subLayer3.image(bgLayer3, 0, 0, width, height);
+    subLayer3.blendMode(LIGHTEST);
+    subLayer3.image(fgLayer3, 0, 0, width, height);
+    blendMode(MULTIPLY);
+    image(subLayer3, 0, 0, width, height);
+    //
+    // blendMode(BLEND);
+    // image(bg, 0, 0, width, height);
 
   } else {
     for (let i = 0; i < tileNum; i++) {
@@ -168,6 +248,8 @@ function draw() {
         image(subLayer1, (width / tileNum) * i, (height / tileNum) * j, width / tileNum, height / tileNum);
         blendMode(DARKEST);
         image(subLayer2, (width / tileNum) * i, (height / tileNum) * j, width / tileNum, height / tileNum);
+        blendMode(DARKEST);
+        image(subLayer3, (width / tileNum) * i, (height / tileNum) * j, width / tileNum, height / tileNum);
       }
     }
     blendMode(BLEND);
--- ui.js
+++ ui.js
@@ -1,7 +1,82 @@
+let swatchCol = ['#004da7', "#d54288", '#fce302', '#004ea9', '#fce300','#007c40', '#479fdf', '#92328c', '#ef6301', '#007b3d', '#fe8f1c', '#fce300'];
+
 let newButton, saveButton;
 let appCol = 0;
-let appBg = '#e3f3fc';
+let appBg = 250;
 
+let fsBool = 0;
+let fsButton;
+
+let swatch1, swatch2, swatch3;
+
+let stage = 0;
+
+let eraseBoolean = 0;
+
+
+
+function calcDimensions() {
+  vW = width / 100;
+
+  if (width > height) {
+    vMax = width / 100;
+    vMin = height / 100;
+  } else {
+    vMax = height / 100;
+    vMin = width / 100;
+  }
+}
+
+function makeSwatch() {
+
+  button = createImg('assets/eraseOn.png');
+  button.remove();
+  button = createImg('assets/eraseOff.png');
+  button.position(1.5 * vMax, height - (14 * vMax));
+  button.size(14 * vMax, 14 * vMax);
+  button.mousePressed(makeErase);
+
+  swatch1 = createButton("");
+  swatch1.position(15 * vMax, height - (13 * vMax));
+  swatch1.size(8 * vMax, 10.5 * vMax);
+  swatch1.style("background-color", swatchCol[((stage*3)+0)]);
+  swatch1.class("box");
+  swatch1.mousePressed(function() {
+    changeBrush(1)
+  });
+
+  swatch2 = createButton("");
+  swatch2.position(23 * vMax, height - (13 * vMax));
+  swatch2.size(8 * vMax, 10.5 * vMax);
+  swatch2.style("background-color", swatchCol[((stage*3)+1)]);
+  swatch2.class("box");
+  swatch2.mousePressed(function() {
+    changeBrush(2)
+  });
+
+  swatch3 = createButton("");
+  swatch3.position(31 * vMax, height - (13 * vMax));
+  swatch3.size(8 * vMax, 10.5 * vMax);
+  swatch3.style('background-color', swatchCol[((stage*3)+2)]);
+  swatch3.class("box");
+  swatch3.mousePressed(function() {
+    changeBrush(3)
+  });
+
+
+  fsButton = createImg('assets/enterFS.png');
+  fsButton.style('height', '4.5vMax');
+  fsButton.position(width-(7.5 * vMax), 1.5 * vMax);
+  fsButton.mousePressed(fs);
+
+  selColour = createImg('assets/colSelected.png');
+  selColour.position(15 * vMax, height - (16 * vMax));
+  selColour.size(8 * vMax, 16 * vMax);
+  selColour.mousePressed();
+
+  saveNext();
+
+}
 
 function saveNext(){
 
@@ -20,52 +95,144 @@ function saveNext(){
   saveButton.mousePressed(saveImg);
 }
 
+
 function nextStep(){
 
+  swatch1.remove();
+  swatch2.remove();
+  swatch3.remove();
+
+  button.remove();
+  selColour.remove();
+
 drawingIsActive = !drawingIsActive;
-
 if (!drawingIsActive){
-
+    makeSlider(winMouseX);
 
 }
 else{
+  stage++;
+  if (stage === 4){
+    stage = 0;
+  }
 bgLayer1.clear();
 bgLayer2.clear();
+bgLayer3.clear();
 subLayer1.background(appBg);
 subLayer2.background(appBg);
+subLayer3.background(appBg);
+console.log(stage);
+
+if (stage === 0){
+  fgLayer1 = loadImage('assets/s1-1.jpg');
+  fgLayer2 = loadImage('assets/s1-2.jpg');
+  fgLayer3 = loadImage('assets/s1-3.jpg');
+}
+else if (stage === 1){
+  fgLayer1 = loadImage('assets/s2-1.jpg');
+  fgLayer2 = loadImage('assets/s2-2.jpg');
+  fgLayer3 = loadImage('assets/s2-3.jpg');
+}
+else if (stage === 2){
+  fgLayer1 = loadImage('assets/s3-1.jpg');
+  fgLayer2 = loadImage('assets/s3-2.jpg');
+  fgLayer3 = loadImage('assets/s3-3.jpg');
+}
+else if (stage === 3){
+  fgLayer1 = loadImage('assets/s4-1.jpg');
+  fgLayer2 = loadImage('assets/s4-2.jpg');
+  fgLayer3 = loadImage('assets/s4-3.jpg');
+}
+
+makeSwatch();
+
+  fsButton.remove();
+saveButton.remove();
+newButton.remove();
+
+currentLayer = 1;
 blendMode(BLEND);
 }
 
 
+
+
 }
 
 
-function saveImg() {
+function changeBrush(layerSelected){
+  currentLayer = layerSelected;
+  console.log(currentLayer);
 
-    blendMode(BLEND);
+  selColour.remove();
+  selColour = createImg('assets/colSelected.png');
+  selColour.position((15+(8*(layerSelected-1)))*vMax, height - (16 * vMax));
+  selColour.size(8 * vMax, 16 * vMax);
+  selColour.mousePressed();
 
-    if (drawingIsActive){
+  button.remove();
+  button = createImg('assets/eraseOff.png');
+  button.position(1.5 * vMax, height - (14 * vMax));
+  button.size(14 * vMax, 14 * vMax);
+  button.mousePressed(makeErase);
 
-  // save the drawing
-    }
-    else{
+  eraseBoolean = 0;
 
-    // save the drawing
-    }
-
-    save('linescape' + month() + day() + hour() + second() + '.jpg');
 }
 
 function makeSlider(_mouseX) {
-
+  
   sliderImg.clear();
-  sliderImg.stroke(255);
-  sliderImg.strokeWeight(5);
-  sliderImg.line(50, height - (6 * vMax), width - (16 * vMax), height - (6 * vMax));
-
+  sliderImg.stroke("#f1b300");
+  sliderImg.strokeWeight(8);
+  sliderImg.line(width * 0.04, height - (6 * vMax), width * 0.82, height - (6 * vMax));
+  sliderImg.stroke("#f1b300");
+  sliderImg.strokeWeight(30);
+  sliderImg.line(50, height - (6 * vMax), constrain(_mouseX, width * 0.04, width * 0.82), height - (6 * vMax));
   sliderImg.rectMode(RADIUS);
-  sliderImg.fill(0);
+  sliderImg.fill("#5cf22c");
   sliderImg.noStroke();
   sliderImg.rect(constrain(_mouseX, width * 0.04, width * 0.82), height - (6 * vMax), 1 * vMax, 5 * vMax);
+
+
+}
+
+function saveImg() {
+    blendMode(BLEND);
+    if (drawingIsActive){
+  // save the drawing
+    }
+    else{
+    // save the drawing
+    }
+    save('linescape' + month() + day() + hour() + second() + '.jpg');
+}
+
+function fs(){
+
+
+ if (!fsBool){
+   fullscreen(1);
+   fsBool = 1;
+ }
+
+ else{
+
+   fullscreen(0);
+   fsBool = 0;
+
+ }
+}
+
+function makeErase(){
+
+  selColour.remove();
+  button.remove();
+  button = createImg('assets/eraseOn.png');
+  button.position(1.5 * vMax, height - (14 * vMax));
+  button.size(14 * vMax, 14 * vMax);
+  button.mousePressed(makeErase);
+
+  eraseBoolean = 1;
 
 }
